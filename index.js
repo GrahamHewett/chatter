3e870db74112c6850bfc072c14fe5ca1de9ab08b
Graham Hewett <hewettgraham@gmail.com>
2019-09-24 17:21:59 +0100

    setup a single room

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-const app = require('express')();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 const port = 3000;
 
-server.listen(port, () => console.log('server is running on port ' + port));
+server.listen(port, () => console.log("server is running on port " + port));
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/public/index.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 // javascript namespace
-const javascript = io.of('javascript');
+const javascript = io.of("/javascript");
 // const react = io.of('react')
 
-javascript.on('connection', (socketServer) => {
-	console.log('user connected');
-	socketServer.on('message', (msg) => {
-		console.log('message ' + msg);
-		javascript.emit('message', msg)
+javascript.on("connection", socketServer => {
+  
+	socketServer.on("join", data => {
+    	socketServer.join(data.room);
+    	javascript
+    		.in(data.room)
+			.emit("message", `new user joined the ${data.room} room`);
 	});
 
-	socketServer.on('disconnect', () => {
-		console.log('user disconnected');
-		javascript.emit('message', 'user disconnected')
-	})
+	socketServer.on("message", data => {
+		console.log("message " + data.msgValue);
+		javascript.in(data.room).emit("message", data.msgValue);
+	});
 
-	// socketServer.emit('message', { boty: 'Hey!, How are you?' })
-	// socketServer.on('anotherEvent', (data) => console.log(data))
-}) 
+	socketServer.on("disconnect", () => {
+		console.log("user disconnected");
+		javascript.emit("message", "user disconnected");
+  	});
 
+  // socketServer.emit('message', { boty: 'Hey!, How are you?' })
+  // socketServer.on('anotherEvent', (data) => console.log(data))
+});
